df73176d066b12aa99c8483c4002071fb209ee2e
Khoren Markosyan <khoren.markosyan@gmail.com>
2022-03-30 22:37:51 +0400

    updated user model

--- lib/features/domain/entities/user.dart
+++ lib/features/domain/entities/user.dart
@@ -5,8 +5,21 @@ class User with _$User {
   const User._();
   const factory User({
     String? login,
-    String? name,
     String? avatarUrl,
+    String? htmlUrl,
+    String? type,
+    String? name,
+    String? company,
+    String? blog,
+    String? location,
+    String? email,
+    String? bio,
+    int? publicRepos,
+    int? publicGists,
+    int? followers,
+    int? following,
+    String? createdAt,
+    String? updatedAt,
   }) = _User;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
